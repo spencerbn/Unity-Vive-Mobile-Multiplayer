8806d230bee3ca23aea9d7250f04d072e97ba8e1
Stiliyan Lazarov <spencrbrown@gmail.com>
2016-05-18 20:47:29 -0400

    controller colliders

--- Assets/Scripts/WandController.cs
+++ Assets/Scripts/WandController.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class WandController : MonoBehaviour {
 
-	// Use this for initialization
+	//This particular wand
+	private SteamVR_TrackedObject trackedObj;
+	private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input ((int)trackedObj.index);}}
+
+			// Use this for initialization
 	void Start () {
-	
+		trackedObj = GetComponent<SteamVR_TrackedObject>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (controller == null) {
+			Debug.Log ("No Controller Found.");
+			return;
+		}
 	}
 }
