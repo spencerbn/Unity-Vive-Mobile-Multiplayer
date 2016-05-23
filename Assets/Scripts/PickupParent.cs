df6a6c48b471ec9120c3798310541afbc867f103
Stiliyan Lazarov <spencrbrown@gmail.com>
2016-05-23 19:46:34 -0400

    pickup parent added

---
+++ Assets/Scripts/PickupParent.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(SteamVR_TrackedObject))]
+public class PickupParent : MonoBehaviour {
+
+	SteamVR_TrackedObject trackedObj;
+	SteamVR_Controller.Device device;
+	void Awake () {
+		trackedObj = GetComponent<SteamVR_TrackedObject> ();
+	}
+
+	void Update(){
+		
+	}
+
+	void FixedUpdate () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			Debug.Log ("Touch down the trigger");
+		}
+	}
+
+	void OnTriggerStay(Collider col){
+		Debug.Log ("Colliding with " + col.name);
+
+		if (device.GetPressDown (SteamVR_Controller.ButtonMask.Trigger)) {
+			col.attachedRigidbody.isKinematic = true;
+			col.transform.position = this.gameObject.transform.position;
+		}
+	}
+}
